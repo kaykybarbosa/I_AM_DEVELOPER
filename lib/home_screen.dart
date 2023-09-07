68249db5f407702ff1aec519724413427535d47c
Kayky Barbosa <kaykyb629@gmail.com>
2023-09-07 08:50:41 -0300

    Adding the Skills and Contacts buttons

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,46 +1,51 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-
 }
 
-class _HomeScreenState extends State<HomeScreen>{
-  
+class _HomeScreenState extends State<HomeScreen> {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 2, 70, 102),
-      appBar: AppBar(
-        title: const Center(
-          child: Text('I AM DEVELOPER'),
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 2, 70, 102),
+        appBar: AppBar(
+          title: const Center(
+            child: Text('I AM DEVELOPER'),
+          ),
         ),
-      ),
-      body: Column(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 15.0,),
             Container(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
               padding: const EdgeInsets.all(10.0),
-              child: const Text('Welcome in my App!',
+              child: const Text(
+                'Welcome in my App!',
                 style: TextStyle(
-                  fontSize: 30.0,
-                  fontFamily: 'Pacifico',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
-                ),
+                    fontSize: 30.0,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
             ),
+            const SizedBox(height: 30.0),
             Container(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 55.0),
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 110.0),
               padding: const EdgeInsets.all(10.0),
-              color: Colors.black,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 14, 90, 153),
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
               child: Row(
                 children: [
-                  const SizedBox(width: 10.0),
+                  const SizedBox(width: 5.0),
                   const Icon(
                     Icons.account_circle,
                     color: Colors.white,
@@ -48,18 +53,79 @@ class _HomeScreenState extends State<HomeScreen>{
                   ),
                   const SizedBox(width: 5.0),
                   TextButton(
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateColor.resolveWith((states) => Colors.white),
+                    onPressed: () {},
+                    child: const Text(
+                      'Profile',
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
                     ),
-                    onPressed: () {}, 
-                    child: const Text('Profile',
-                      style: TextStyle(
-                        fontSize: 20.0
-                      ),)
                   ),
-                ]),
-            )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 110.0),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 14, 90, 153),
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
+              child: Row(
+                children: [
+                  const SizedBox(width: 5.0),
+                  const Icon(
+                    Icons.construction_rounded,
+                    color: Colors.white,
+                    size: 40.0,
+                  ),
+                  const SizedBox(width: 5.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.pressed)) {
+                            return Colors.red;
+                          }
+                          return const Color.fromARGB(255, 14, 90, 153);
+                        },
+                      ),
+                    ),
+                    child: const Text(
+                      'Skills',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 110.0),
+              padding: const EdgeInsets.all(10.0),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 14, 90, 153),
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
+              child: Row(
+                children: [
+                  const SizedBox(width: 5.0),
+                  const Icon(
+                    Icons.contact_mail_rounded,
+                    color: Colors.white,
+                    size: 40.0,
+                  ),
+                  const SizedBox(width: 5.0),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Contacts',
+                        style: TextStyle(fontSize: 20.0, color: Colors.white)),
+                  ),
+                ],
+              ),
+            ),
           ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,193 +3,209 @@ import 'package:i_am_developer/home_screen.dart';
 
 void main() => runApp(const IAmDeveloper());
 
-class IAmDeveloper extends StatelessWidget{
+class IAmDeveloper extends StatelessWidget {
   const IAmDeveloper({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'I Am Developer',
       initialRoute: '/home',
       routes: {
-        '/': (context) => const IAmDeveloperPage(),
+        '/dev': (context) => const IAmDeveloperPage(),
         '/home': (context) => const HomeScreen(),
       },
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 2, 70, 102))
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 2, 70, 102),
+        ),
       ),
     );
   }
 }
 
-class IAmDeveloperPage extends StatefulWidget{
+class IAmDeveloperPage extends StatefulWidget {
   const IAmDeveloperPage({super.key});
 
   @override
   State<IAmDeveloperPage> createState() => _IAmDeveloperState();
 }
 
-class _IAmDeveloperState extends State<IAmDeveloperPage>{
-  
+class _IAmDeveloperState extends State<IAmDeveloperPage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 2, 70, 102),
-        appBar: AppBar(
-          title: const Text('I AM DEVELOPER',
-            style: TextStyle(
-              fontFamily: 'Lora',
-            ),
+      backgroundColor: const Color.fromARGB(255, 2, 70, 102),
+      appBar: AppBar(
+        title: const Text(
+          'I AM DEVELOPER',
+          style: TextStyle(
+            fontFamily: 'Lora',
           ),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 4, 71, 143),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 10.0, top: 10.0),
-              child: const Row(
-                children: [
-                  CircleAvatar(
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 4, 71, 143),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 10.0, top: 10.0),
+            child: const Row(
+              children: [
+                CircleAvatar(
                     backgroundImage: AssetImage('assets/my_face/kayky.jpg'),
                     radius: 35.0),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('Kayky Barbosa',
-                    style: TextStyle(
+                SizedBox(width: 10.0),
+                Text(
+                  'Kayky Barbosa',
+                  style: TextStyle(
                       fontSize: 30.0,
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Poppins'),
-                  ),
-                ],
-              ),
-            ),
-            const Row(
-              children: [
-                SizedBox(
-                  width: 110.0,
                 ),
-                Text('Back-End Developer',
-                  style: TextStyle(
+              ],
+            ),
+          ),
+          const Row(
+            children: [
+              SizedBox(width: 110.0),
+              Text(
+                'Back-End Developer',
+                style: TextStyle(
                     color: Colors.white,
                     fontSize: 18.0,
                     fontStyle: FontStyle.italic,
                     fontFamily: 'Lora'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 25.0),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'SOME OF THE TECHNOLOGIES I USE:',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'Lora',
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 15.0),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('assets/imagens/java.png'),
+                width: 70,
+              ),
+              Image(
+                image: AssetImage('assets/imagens/spring.png'),
+                width: 70,
+              ),
+              Image(
+                image: AssetImage('assets/imagens/cshap.png'),
+                width: 70,
+              ),
+              Image(
+                image: AssetImage('assets/imagens/dotnet.png'),
+                width: 70,
+              ),
+              Image(
+                image: AssetImage('assets/imagens/python.png'),
+                width: 70,
+              ),
+            ],
+          ),
+          const SizedBox(height: 10.0),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('assets/imagens/dart.png'),
+                width: 70,
+              ),
+              Image(
+                image: AssetImage('assets/imagens/flutter.png'),
+                width: 70,
+              ),
+              Image(
+                image: AssetImage('assets/imagens/html.png'),
+                width: 70,
+              ),
+              Image(
+                image: AssetImage('assets/imagens/css.png'),
+                width: 70,
+              ),
+              Image(
+                image: AssetImage('assets/imagens/docker.png'),
+                width: 70,
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 25.0, top: 55.0),
+            child: const Row(
+              children: [
+                Text(
+                  'Contact:',
+                  style: TextStyle(fontSize: 22.0, color: Colors.white),
                 ),
               ],
             ),
-            const SizedBox(
-              height: 25.0,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Container(
+            margin:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            color: Colors.white,
+            padding: const EdgeInsets.all(10.0),
+            child: const Row(
               children: [
+                Icon(
+                  Icons.phone,
+                  size: 25.0,
+                  color: Color.fromARGB(255, 2, 70, 102),
+                ),
+                SizedBox(width: 10.0),
                 Text(
-                  'SOME OF THE TECHNOLOGIES I USE:',
+                  '+55 98 98606-4174',
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontFamily: 'Lora',
-                  ),
+                      color: Color.fromARGB(255, 2, 70, 102),
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
                 )
               ],
             ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(10.0),
+            margin:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            child: const Row(
               children: [
-                Image(image: AssetImage('assets/imagens/java.png'), width: 70,),
-                Image(image: AssetImage('assets/imagens/spring.png'), width: 70,),
-                Image(image: AssetImage('assets/imagens/cshap.png'), width: 70,),
-                Image(image: AssetImage('assets/imagens/dotnet.png'), width: 70,),
-                Image(image: AssetImage('assets/imagens/python.png'), width: 70,),
-              ],
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(image: AssetImage('assets/imagens/dart.png'), width: 70,),
-                Image(image: AssetImage('assets/imagens/flutter.png'), width: 70,),
-                Image(image: AssetImage('assets/imagens/html.png'), width: 70,),
-                Image(image: AssetImage('assets/imagens/css.png'), width: 70,),
-                Image(image: AssetImage('assets/imagens/docker.png'), width: 70,),
-              ],
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 25.0, top: 55.0),
-              child: const Row(
-                children: [
-                  Text('Contact:',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      color: Colors.white
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              color: Colors.white,
-              padding: const EdgeInsets.all(10.0),
-              child: const Row(
-                children: [
-                  Icon(
-                    Icons.phone,
-                    size: 25.0,
-                    color:Color.fromARGB(255, 2, 70, 102),
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('+55 98 98606-4174',
-                    style: TextStyle(
-                      color:Color.fromARGB(255, 2, 70, 102),
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.email,
-                    size: 25.0,
-                    color:Color.fromARGB(255, 2, 70, 102),
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('contatokaykybarbosa@gmail.com',
-                    style: TextStyle(
-                      color:Color.fromARGB(255, 2, 70, 102),
+                Icon(
+                  Icons.email,
+                  size: 25.0,
+                  color: Color.fromARGB(255, 2, 70, 102),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'contatokaykybarbosa@gmail.com',
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 2, 70, 102),
                       fontSize: 18.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                ],
-              ),
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.pushNamed(context, '/home');
-              },
-              child: const Text('Go to the Home'),),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
